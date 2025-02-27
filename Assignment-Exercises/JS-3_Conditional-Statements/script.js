ba22cb775b8e9bf94a4701e867c0ad78d52f1ce0
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-02-27 11:34:42 +0800

    fix(js-3): improve result display for valid and invalid age input

--- Assignment-Exercises/JS-3_Conditional-Statements/script.js
+++ Assignment-Exercises/JS-3_Conditional-Statements/script.js
@@ -3,28 +3,32 @@ const ageInput = document.getElementById("ageInput");
 
 function checkAge() {
 
-    let age = ageInput.value;
+    let age = Number(ageInput.value);
 
     if(age >= 0 && age <= 12){
             
         // I didn't output "a/an" because I want to keep my output clean.
         result.textContent = "You are a: Child";
+
+        // Removes the error styling.
+        result.classList.remove("error");
     }
 
     else if(age >= 13 && age <= 19){
         result.textContent = "You are a: Teenager";
+        result.classList.remove("error");
     }
 
     else if(age >= 20){
         result.textContent = "You are an: Adult";
+        result.classList.add("result");
+        result.classList.remove("error");
     }
 
     else{
         result.textContent = "Invalid age. Please enter a valid number.";
-        result.style.color = "red";
-        result.style.backgroundColor = "#ffcfcf";
+
+        // Applying the CSS properties and values for error messages.
+        result.classList.add("error");
     }
-
 }
-
-checkAge();

db5e2486587a143bbd97e22102ca92a9a06fbcf9
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-02-27 11:22:09 +0800

    feat(js-3): complete JS assignment 3

--- Assignment-Exercises/JS-3_Conditional-Statements/script.js
+++ Assignment-Exercises/JS-3_Conditional-Statements/script.js
@@ -1,3 +1,30 @@
+const result = document.getElementById("result");
+const ageInput = document.getElementById("ageInput");
+
 function checkAge() {
-    // write your code here 
+
+    let age = ageInput.value;
+
+    if(age >= 0 && age <= 12){
+            
+        // I didn't output "a/an" because I want to keep my output clean.
+        result.textContent = "You are a: Child";
+    }
+
+    else if(age >= 13 && age <= 19){
+        result.textContent = "You are a: Teenager";
+    }
+
+    else if(age >= 20){
+        result.textContent = "You are an: Adult";
+    }
+
+    else{
+        result.textContent = "Invalid age. Please enter a valid number.";
+        result.style.color = "red";
+        result.style.backgroundColor = "#ffcfcf";
+    }
+
 }
+
+checkAge();

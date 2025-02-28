63caf78dbc0e73713e56800e846adc1fe034bdee
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-02-28 18:09:49 +0800

    feat(js-8): complete JS assignment 8

--- Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
+++ Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
@@ -1,1 +1,20 @@
 // your code starts here
+
+const counter = document.querySelector("#counter-text");
+const subtract = document.querySelector("#button-subtract");
+const add = document.querySelector("#button-add");
+
+
+let counterVal = 0;
+
+subtract.addEventListener("click", event => {
+    
+                        // Decrement the value
+    counter.textContent = --counterVal;
+});
+
+add.addEventListener("click", event => {
+
+                        // Increment the value
+    counter.textContent = ++counterVal;
+});
